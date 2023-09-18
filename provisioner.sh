6620f8b8b8776451c96fb7b845e91c4acb778425
ch <mkmsacc@outlook.com>
2023-09-18 21:24:51 +0700

    Add install curl

--- provisioner.sh
+++ provisioner.sh
@@ -3,6 +3,7 @@
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 sudo apt autoremove -y
+sudo apt install curl -y
 curl -fsSL https://test.docker.com | sudo sh
 sudo usermod -aG docker vagrant
 sudo systemctl enable docker.service
